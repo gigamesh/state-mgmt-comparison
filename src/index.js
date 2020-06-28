7a4cf0cf0e51f84d555ba8bbe495125f399a7295
Matthew Masurka <m.masurka@gmail.com>
2020-06-28 14:49:38 -0500

    Finishes initial setup

--- src/index.js
+++ src/index.js
@@ -1,14 +1,49 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./styles/tailwind.generated.css";
+import MobXApp from "./Apps/MobXApp";
+import ContextApp from "./Apps/ContextApp";
+import XStateApp from "./Apps/XStateApp";
+import * as serviceWorker from "./serviceWorker";
+
+const mobxPanel = document.getElementById("root-mobx");
+const contextPanel = document.getElementById("root-context");
+const xStatePanel = document.getElementById("root-xstate");
+
+const panels = [mobxPanel, contextPanel, xStatePanel];
+
+const nav = document.getElementById("app-nav");
+
+nav.addEventListener("click", (e) => {
+  const { id } = e.target.dataset;
+  panels.forEach((panel) => {
+    if (panel.id === id) {
+      panel.style.display = "block";
+    } else {
+      panel.style.display = "none";
+    }
+  });
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <MobXApp />
   </React.StrictMode>,
-  document.getElementById('root')
+  mobxPanel
+);
+
+ReactDOM.render(
+  <React.StrictMode>
+    <ContextApp />
+  </React.StrictMode>,
+  contextPanel
+);
+
+ReactDOM.render(
+  <React.StrictMode>
+    <XStateApp />
+  </React.StrictMode>,
+  xStatePanel
 );
 
 // If you want your app to work offline and load faster, you can change
